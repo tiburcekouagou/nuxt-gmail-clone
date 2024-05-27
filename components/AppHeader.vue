3a532d99bdf819e5f75fa0a9554444f446dd3ba5
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-05-27 21:26:41 +0100

    init project

---
+++ components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <img src="~/assets/img/logo_gmail.png">
+
+    <input type="search" />
+    
+  </header>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <AppHeader />
+    <h1>Let's build gmail clone</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
