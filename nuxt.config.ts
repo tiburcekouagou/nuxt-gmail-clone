75c32a62f3da038c9d67bab71b1554c6efef3e2a
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-05-28 13:57:29 +0200

    changed favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,10 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [],
+  app: {
+    head: {
+      // link: [{rel: 'icon', href: '/gmail.ico'}],
+      title: 'Gmail Clone'
+    }
+  }
 })
