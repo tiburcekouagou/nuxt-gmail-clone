4c8308f1b48b1967f831307c3114db98e856fc6a
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-05-28 14:00:32 +0200

    installed tailwindcss and icon modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
   app: {
     head: {
       // link: [{rel: 'icon', href: '/gmail.ico'}],
